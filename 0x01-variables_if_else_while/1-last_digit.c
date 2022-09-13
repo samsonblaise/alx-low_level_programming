220339fe915109786f6f99128bce96d1e4f70b25
samsonblaise <ach.samsonameh@gmail.com>
2022-09-13 17:39:19 +0000

    This program prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,5 @@
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there*/
 /**
  * main - Entry point
  *
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints entry point
+ * Return: Alwyas 0 (successful)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'b';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
