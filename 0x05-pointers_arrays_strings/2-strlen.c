5220f52117efd5d77e9012f67ff127b62789338b
samsonblaise <ach.samsonameh@gmail.com>
2022-09-21 02:41:44 +0000

    Return length as string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: lent of string as integer
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
