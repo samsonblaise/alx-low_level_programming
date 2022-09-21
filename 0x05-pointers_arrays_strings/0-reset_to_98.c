18d550e5c092a7753e4cf3203f2e9e1978d8515d
samsonblaise <ach.samsonameh@gmail.com>
2022-09-21 02:22:31 +0000

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that update the  value of a parameter it points to 98
+ * @n: parameter
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
